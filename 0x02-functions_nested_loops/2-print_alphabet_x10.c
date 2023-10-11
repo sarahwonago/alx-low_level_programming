6aaae57b3aa0fa7f14b35e13d8697dff42ee6a34
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-10-11 14:15:30 +0300

    functions and nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10(void): prints 10 times the alphabet.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int round = 0;
+	char letter = 'a';
+
+	while (round < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		round++;
+	}
+}
