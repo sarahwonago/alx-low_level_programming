d4682c3ba6993799511989497a46e76336b9bc0e
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-10-11 14:08:49 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,25 @@
-#include "main.h";
+#include "main.h"
+#include <stdio.h>
 
 /**
- * main - prints _putchar
+ * main - print putchar
  *
- * Description: prints putchar
+ * Description: print putchar
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
 
-    return(0);
+        return(0);
 }
