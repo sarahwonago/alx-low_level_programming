44863495374c8c45c33fc05d2a92b9e29dd88f75
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-10-24 17:21:56 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - prints combination of numbers
+ * Description: code to print combination of numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
     int num1, num2;
 
-    for (num1 = 0; num1 <= 98; num1++) {
-        for (num2 = num1 + 1; num2 <= 99; num2++) {
+    for (num1 = 0; num1 <= 98; num1++)
+    {
+        for (num2 = num1 + 1; num2 <= 99; num2++)
+        {
             putchar((num1 / 10) + '0');    // Print the tens digit of num1
             putchar((num1 % 10) + '0');    // Print the ones digit of num1
             putchar(' ');                   // Print a space
             putchar((num2 / 10) + '0');    // Print the tens digit of num2
             putchar((num2 % 10) + '0');    // Print the ones digit of num2
 
-            if (num1 != 98 || num2 != 99) {
+            if (num1 != 98 || num2 != 99)
+            {
                 putchar(',');  // Print a comma
                 putchar(' ');  // Print a space
             }
@@ -19,5 +29,5 @@ int main(void) {
     }
 
     putchar('\n');
-    return 0;
+    return (0);
 }
