7ef994cb5a2cdf1365fc111155fe0462f329932d
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-10-24 16:53:13 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,14 +10,18 @@
 int main(void) {
     int digit1, digit2, digit3;
 
-    for (digit1 = 0; digit1 <= 7; digit1++) {
-        for (digit2 = digit1 + 1; digit2 <= 8; digit2++) {
-            for (digit3 = digit2 + 1; digit3 <= 9; digit3++) {
+    for (digit1 = 0; digit1 <= 7; digit1++)
+    {
+        for (digit2 = digit1 + 1; digit2 <= 8; digit2++)
+        {
+            for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
+            {
                 putchar(digit1 + '0');
                 putchar(digit2 + '0');
                 putchar(digit3 + '0');
 
-                if (digit1 != 7 || digit2 != 8 || digit3 != 9) {
+                if (digit1 != 7 || digit2 != 8 || digit3 != 9)
+                {
                     putchar(',');
                     putchar(' ');
                 }
@@ -26,5 +30,5 @@ int main(void) {
     }
 
     putchar('\n');
-    return 0;
+    return (0);
 }
