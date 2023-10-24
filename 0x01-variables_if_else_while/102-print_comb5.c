ea2fe4af0ed10648c6dd01550bc00d0d031ca122
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-10-24 17:20:56 +0300

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+    int num1, num2;
+
+    for (num1 = 0; num1 <= 98; num1++) {
+        for (num2 = num1 + 1; num2 <= 99; num2++) {
+            putchar((num1 / 10) + '0');    // Print the tens digit of num1
+            putchar((num1 % 10) + '0');    // Print the ones digit of num1
+            putchar(' ');                   // Print a space
+            putchar((num2 / 10) + '0');    // Print the tens digit of num2
+            putchar((num2 % 10) + '0');    // Print the ones digit of num2
+
+            if (num1 != 98 || num2 != 99) {
+                putchar(',');  // Print a comma
+                putchar(' ');  // Print a space
+            }
+        }
+    }
+
+    putchar('\n');
+    return 0;
+}
