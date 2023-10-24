7655962ae3f2931ed9a896df1c6b3789bb1765c7
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-10-24 17:11:30 +0300

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int firstDigit, secondDigit;
+
+    for (firstDigit = 0; firstDigit <= 8; firstDigit++)
+    {
+        for (secondDigit = firstDigit + 1; secondDigit <= 9; secondDigit++)
+        {
+            putchar(firstDigit + '0');
+            putchar(secondDigit + '0');
+
+            if (firstDigit != 8 || secondDigit != 9)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    putchar('\n');
+    return (0);
+}
