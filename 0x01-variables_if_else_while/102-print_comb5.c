551d5d9e16e80fe0df407c66ee0f6e3c88e3e531
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-10-24 17:23:21 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,16 +14,16 @@ int main(void)
     {
         for (num2 = num1 + 1; num2 <= 99; num2++)
         {
-            putchar((num1 / 10) + '0');    // Print the tens digit of num1
-            putchar((num1 % 10) + '0');    // Print the ones digit of num1
-            putchar(' ');                   // Print a space
-            putchar((num2 / 10) + '0');    // Print the tens digit of num2
-            putchar((num2 % 10) + '0');    // Print the ones digit of num2
+            putchar((num1 / 10) + '0');
+            putchar((num1 % 10) + '0');
+            putchar(' ');
+            putchar((num2 / 10) + '0');
+            putchar((num2 % 10) + '0');
 
             if (num1 != 98 || num2 != 99)
             {
-                putchar(',');  // Print a comma
-                putchar(' ');  // Print a space
+                putchar(',');
+                putchar(' ');
             }
         }
     }
