a4b8fdab37c2aaad29f34397b5bd6f3f0a304d00
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-10-28 19:53:13 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,19 +5,23 @@
  * @size: input integer
  * Return: 0
  */
+
 void print_square(int size)
 {
-	int a, b;
+    if (size <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
 
-	if (size > 0)
-	{
-		for (a = 0; a < size; a++)
-		{
-			for (b = 0; b < size; b++)
-				_putchar('#');
-			_putchar('\n');
-		}
-	}
-	else
-		_putchar('\n');
+    int row, column;
+
+    for (row = 0; row < size; row++)
+    {
+        for (column = 0; column < size; column++)
+        {
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
 }
