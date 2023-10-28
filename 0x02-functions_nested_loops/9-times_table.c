d44de71ee48a995e023e1f985079f3d39f2ccad8
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-10-28 13:30:55 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,35 +10,30 @@
 
 void times_table(void)
 {
-	int row, column, product, tens, ones;
+    int row, column, product;
 
-	for (row = 0; row <= 9; row++)
-	{
-		for (column = 0; column <= 9; column++)
-		{
-			product = row * column;
-			tens = product / 10;
-			ones = product % 10;
+    for (row = 0; row <= 9; row++)
+    {
+        for (column = 0; column <= 9; column++)
+        {
+            product = row * column;
 
-			if (column == 0)
-			{
-				_putchar('0');
-			}
-			else if (product < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(ones + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(tens + '0');
-				_putchar(ones + '0');
-			}
-		}
-		_putchar('\n');
-	}
+            if (column == 0)
+                _putchar(product + '0');
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+
+                if (product < 10)
+                    _putchar(' ');
+
+                if (product >= 10)
+                    _putchar((product / 10) + '0');
+
+                _putchar((product % 10) + '0');
+            }
+        }
+        _putchar('\n');
+    }
 }
