8eff9d8c332f04517b2047483fcc8a044f6715a2
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-10-28 18:54:16 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,13 +8,12 @@
  */
 int _isdigit(int c)
 {
-	char num = '0';
-	int dgt = 0;
-
-	for (; num <= '9'; num++)
+	if (c >= '0' && c <= '9')
 	{
-		if (num == c)
-			dgt = 1;
+		return (1);
 	}
-	return (dgt);
+	else
+	{
+		return (0); 
+	}
 }
