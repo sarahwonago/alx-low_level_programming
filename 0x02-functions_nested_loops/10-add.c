7e6c928ad639bbe5ce7fa08a38a64e877e6331df
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-10-28 13:45:12 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
  * add - adds two integers
- * @n1: integer 1
- * @n2: integer 2
+ * @num1: integer 1
+ * @num2: integer 2
  *
  * Description: give two integers, returns sum
  *
  * Return: sum
  */
 
-int add(int n1, int n2)
+int add(int num1, int num2)
 {
 	int sum;
 
-	sum = n1 + n2;
+	sum = num1 + num2;
 
 	return (sum);
 }
