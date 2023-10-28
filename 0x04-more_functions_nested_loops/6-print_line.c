bc6d72b50687ec2d3093b42fd26a9e477af59107
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-10-28 19:35:08 +0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,14 +5,21 @@
  * @n: input integer
  * Return: 0
  */
+
 void print_line(int n)
 {
-	int a = 0;
-
-	if (n > 0)
-	{
-		for (; a < n; a++)
-			_putchar('_');
-	}
-	_putchar('\n');
+    if (n <= 0)
+    {
+        _putchar('\n');
+    }
+    else
+    {
+        while (n > 0)
+        {
+            _putchar('_');
+            n--;
+        }
+        _putchar('\n');
+    }
 }
+
