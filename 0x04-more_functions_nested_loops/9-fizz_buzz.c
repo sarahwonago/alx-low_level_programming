23f8ab4a9fd7a4722855b76fd013f72685539196
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-10-28 19:58:32 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,21 +5,28 @@
  * main - prints numbers between 1 and 100
  * Return: 0
  */
+
 int main(void)
 {
-	int n;
+    int i;
 
-	for (n = 1; n < 100; n++)
-	{
-		if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz ");
-		else if (n % 3 == 0)
-			printf("Fizz ");
-		else if (n % 5 == 0)
-			printf("Buzz ");
-		else
-			printf("%d ", n);
-	}
-	printf("Buzz\n");
-	return (0);
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+            printf("FizzBuzz");
+        else if (i % 3 == 0)
+            printf("Fizz");
+        else if (i % 5 == 0)
+            printf("Buzz");
+        else
+            printf("%d", i);
+
+        if (i < 100)
+            printf(" ");
+        else
+            printf("\n");
+    }
+
+    return (0);
 }
+
