296e8ad82e4d152385171ac8037bb48a1288e757
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-10-28 19:10:40 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,19 @@
  * print_numbers - prints numbers 0 to 9
  * Return: 0
  */
+
 void print_numbers(void)
 {
-	int num = '0';
+    char n = '0';
 
-	for (; num <= '9'; num++)
-		_putchar(num);
-	_putchar('\n');
+    while (n <= '9')
+    {
+        _putchar(n);
+        n++;
+    }
+
+    _putchar('\n');
 }
+
 
 
