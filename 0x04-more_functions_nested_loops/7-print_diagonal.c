d3b5fdb1f3ef808d82adabdfffdaa5c8658c8933
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-10-28 19:43:37 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,20 +5,28 @@
  * @n: input integer
  * Return: nothing
  */
+
 void print_diagonal(int n)
 {
-	int a = 0, b;
+    if (n <= 0)
+    {
+        _putchar('\n');
+    }
+    else
+    {
+        int spaces = 0;
+        while (n > 0)
+        {
+            int i;
 
-	if (n > 0)
-	{
-		for (; a < n; a++)
-		{
-			for (b = 0; b < a; b++)
-				_putchar(' ');
-			_putchar(92);
-			_putchar('\n');
-		}
-	}
-	else
-		_putchar('\n');
+            for (i = 0; i < spaces; i++)
+            {
+                _putchar(' ');
+            }
+            _putchar('\\');
+            _putchar('\n');
+            spaces++;
+            n--;
+        }
+    }
 }
