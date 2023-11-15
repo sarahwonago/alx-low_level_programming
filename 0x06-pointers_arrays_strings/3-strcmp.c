16ced3dab0281c5923dcec3e3f215b223d0670d1
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-11-15 17:03:04 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two strings
- * @s1: first string
- * @s2: string 2
- * Return: 0 if s1 and s2 are the same and 1 if not
+ * _strcmp - Compares two strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: 0 if s1 and s2 are the same, positive if s1 is greater,
+ *         and negative if s2 is greater
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -12,10 +14,11 @@ int _strcmp(char *s1, char *s2)
 
 	while (k == 0)
 	{
-		if ((*(s1 + j) == '\0') && (*(s2 + j) == '\0'))
+		if ((s1[j] == '\0') && (s2[j] == '\0'))
 			break;
-		k = *(s1 + j) - *(s2 + j);
+		k = s1[j] - s2[j];
 		j++;
 	}
+
 	return (k);
 }
