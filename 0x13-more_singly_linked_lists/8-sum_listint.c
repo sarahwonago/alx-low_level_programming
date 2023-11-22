97fd44b8092b64516f665bd959f742c218104cac
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-11-22 14:12:10 +0300

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 
 /**
- * sum_listint - calculates the sum of all the data in a listint_t list
+ * sum_listint - calculates the sum of all the data in listint_t list
  * @head: first node in the linked list
  *
  * Return: resulting sum
  */
+
 int sum_listint(listint_t *head)
 {
 	int sum;
