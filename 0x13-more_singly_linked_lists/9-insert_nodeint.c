7c3d42ad520842a20d9e432c317b99c6f0513322
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-11-22 14:12:43 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list,
+ * insert_nodeint_at_index - inserts a new node in linked list,
  * at a given position
  * @head: pointer to the first node in the list
  * @idx: index where the new node is added
@@ -9,6 +9,7 @@
  *
  * Return: pointer to the new node, or NULL
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *copy = *head;
