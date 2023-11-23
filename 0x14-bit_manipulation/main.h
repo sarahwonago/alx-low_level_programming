f3fd5e413d1e1e93d39b2a2aad08eeed8445f75a
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-11-23 19:27:39 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,15 +1,12 @@
-#ifndef __BITS__
-#define __BITS__
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 #endif
