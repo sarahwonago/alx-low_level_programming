ab7e524f64982f1609d80e367da05961636f4ed8
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-11-23 19:08:40 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,20 +8,20 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int c;
-	int len, bin;
+	int len, bb;
 
 	if (!b)
 		return (0);
 	c = 0;
 	for (len = 0; b[len] != '\0'; len++)
 		;
-	for (len--, bin = 1; len >= 0; len--, bin *= 2)
+	for (len--, bb = 1; len >= 0; len--, bb *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
 
 		if (b[len] & 1)
-			c += bin;
+			c += bb;
 	}
 	return (c);
 }
