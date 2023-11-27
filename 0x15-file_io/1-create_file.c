8805681e41a2ef793db5bbc415d0a3ad641ad16d
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-11-27 15:12:35 +0300

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,30 +9,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int f;
+	int letters;
+	int rw;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd == -1)
+	if (f == -1)
 		return (-1);
 
 	if (!text_content)
 		text_content = "";
 
-	for (nletters = 0; text_content[nletters]; nletters++)
+	for (letters = 0; text_content[letters]; letters++)
 		;
 
-	rwr = write(fd, text_content, nletters);
+	rw = write(f, text_content, letters);
 
-	if (rwr == -1)
+	if (rw == -1)
 		return (-1);
 
-	close(fd);
+	close(f);
 
 	return (1);
 }
