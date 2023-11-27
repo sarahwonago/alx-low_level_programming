7938e0ea99757084d71eac9f1793967eee62efd6
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-11-27 15:10:12 +0300

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,28 +9,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	int f;
+	ssize_t nr, nw;
+	char *buff;
 
 	if (!filename)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	f = open(filename, O_RDONLY);
 
-	if (fd == -1)
+	if (f == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	buff = malloc(sizeof(char) * (letters));
+	if (!buff)
 		return (0);
 
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
+	nr = read(f, buff, letters);
+	nw = write(STDOUT_FILENO, buff, nr);
 
-	close(fd);
+	close(f);
 
-	free(buf);
+	free(buff);
 
-	return (nwr);
+	return (nw);
 }
