97d75a9a2c770bb84015017c5395d94c1fa48dab
sarahwonago <sarahshaaleh@gmail.com>
2024-02-12 13:21:07 +0300

    corrections

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,12 +12,19 @@
 int print_last_digit(int n)
 {
 
+	int last_digit;
+
 	if (n < 0)
+	{
 		n = -n;
-
-	int last = n % 10;
+		last_digit = n % 10;
+	}
+	else
+	{
+		last_digit = n % 10;
+	}
 	
-	_putchar(last + '0');
+	_putchar(last_digit + '0');
 
-	return (last);
+	return (last_digit);
 }
