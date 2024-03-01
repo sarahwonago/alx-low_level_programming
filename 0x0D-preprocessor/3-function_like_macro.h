d7734c554b8d5cea090f70a132d88e129e703582
sarahwonago <sarahshaaleh@gmail.com>
2024-03-01 13:11:09 +0300

    corrections

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
+/*#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x)) */
+#define ABS(X) ((X > 0) ? (X) : (-X))
 
-#endif
+#endif /* ifndef FUNCTION_LIKE_MACRO_H */
